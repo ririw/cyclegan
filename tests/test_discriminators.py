c85aa4e92778162d13a304e0236656876bbfa2a6
Richard Weiss <richardweiss@richardweiss.org>
2018-12-28 18:05:58 +1100

    Unit tests now consistently pass

--- tests/test_discriminators.py
+++ tests/test_discriminators.py
@@ -9,14 +9,15 @@ from cyclegan import datasets, discriminators
 def test_mnist_is_zero() -> None:
     trf = discriminators.MNISTDiscriminator()
 
+    train_sample = np.random.choice(len(datasets.MNIST), size=32)
+    raw_x = datasets.MNIST.train_data[train_sample]
+    raw_y = (datasets.MNIST.train_labels[train_sample] == 0)
+    x = raw_x.type(torch.float32) / 255
+    y = raw_y.type(torch.float32)
+
     trf_opt = optim.Adam(trf.parameters())
     errs = []
-    for _ in range(20):
-        train_sample = np.random.choice(len(datasets.MNIST), size=128)
-
-        x = datasets.MNIST.train_data[train_sample].type(torch.float32) / 255
-        y = (datasets.MNIST.train_labels[train_sample] == 0).type(torch.float32)
-
+    for _ in range(64):
         trf_opt.zero_grad()
         y_gen = trf.forward(x)
         err = nn.MSELoss()(y_gen, y)
